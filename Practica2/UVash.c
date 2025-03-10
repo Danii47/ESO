6806a95c75e7503538fd0a329e38e4a4c802c8f3
Danii47 <uranux1@gmail.com>
2025-03-10 03:22:53 +0100

    add redirections

--- Practica2/UVash.c
+++ Practica2/UVash.c
@@ -4,9 +4,12 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
 int main(int argc, char* argv[]) {
 	FILE* read_file = NULL;
+        char* input_file = NULL;
+	char* output_file = NULL;
 	char* line = NULL;
 	size_t size = 0;
 
@@ -35,10 +38,17 @@ int main(int argc, char* argv[]) {
 		line[strcspn(line, "\n")] = '\0';
 		char *ptr = line;
 		while ((token = strsep(&ptr, " ")) != NULL) {
-			if (*token != '\0') {
+			if (strcmp(token, ">") == 0) {  // Redirección de salida
+                		output_file = strsep(&ptr, " ");
+            		
+			} else if (strcmp(token, "<") == 0) {  // Redirección de entrada
+                		input_file = strsep(&ptr, " ");
+			
+			} else if (token[0] != '\0') {
 				args[i++] = token;
 			}
 		}
+
 		args[i] = NULL;
 
 		if (strcmp(args[0], "exit") == 0) {
@@ -60,6 +70,26 @@ int main(int argc, char* argv[]) {
 				perror("fork");
 				return 1;
 			} else if (pid == 0) {
+				if (input_file) {
+					int fd_in = open(input_file, O_RDONLY);
+					if (fd_in == -1) {
+					    perror("Error abriendo archivo de entrada");
+					    exit(1);
+					}
+					dup2(fd_in, STDIN_FILENO);
+					close(fd_in);
+				}
+
+				if (output_file) {
+					int fd_out = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+					if (fd_out == -1) {
+					    perror("Error abriendo archivo de salida");
+					    exit(1);
+                			}
+                			dup2(fd_out, STDOUT_FILENO);
+                			close(fd_out);
+            			}
+				
 				execvp(args[0], args);
 				fprintf(stderr, "An error has occurred\n");
 				exit(1);
@@ -67,7 +97,9 @@ int main(int argc, char* argv[]) {
 				waitpid(pid, NULL, 0);
     			}
 		}
-
+		
+		input_file = NULL;
+		output_file = NULL;
 		free(line);
 		size = 0;
 		i = 0;
